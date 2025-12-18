b42f8da6e9e30d000994d425f42a486e885d1ee4
retr0inv4der <retr0.inv4der@gmail.com>
2025-12-18 22:50:50 +0330

    better handling

--- telegram-selfbot/main.py
+++ telegram-selfbot/main.py
@@ -1,30 +1,45 @@
 import asyncio
-from pyrogram import Client
+from pyrogram import Client 
+from pyrogram.enums import ParseMode
+
 import os
 import dotenv
 from math import *
+from datetime import date , datetime
 
 dotenv.load_dotenv()
 api_id = os.getenv("TEL_API_ID")
 api_hash = os.getenv("TEL_API_HASH")
-
-app = Client("my_account")
+proxy = {
+    "scheme": "MTPROTO",  # "socks4", "socks5" and "http" are supported
+    "hostname": "65.109.147.218",
+    "port": 433,
+    "credentials" : "EERighJJvXrFGRMCIMJdCQ"
+}
+app = Client("my_account" )
 
 @app.on_message()
-async def handle_message(client, message):
+async def handle_message(client, message ):
     # check if the "calc" string is in the message text and then calculate the expression after it and reply a message to the message sender
     #handle the uppercase and lowercase of "calc"
-    if "calc" in message.text.lower():
-        message.text = message.text.lower()
-        expression = message.text.split("calc",1)[1].strip()
-        try:
-            result = eval(expression)
-            await message.reply_text(f"The result is: {result}")
-        except Exception as e:
-            await message.reply_text(f"Error in calculation: {e}")
-            pass
-    print(f"Received message: {message.text}")
+    #check if the sender is me
+    if message.from_user.id == (await app.get_me()).id:
+        
+        if "/run" in message.text.lower():
+            message.text = message.text.lower()
+            expression = message.text.split("/run",1)[1].strip()
+            try:
+                result = eval(expression)
+                #edit my message instead of sending a new one
+                #put the messge in markdown format
+                await message.edit(f"**{expression}:**\n ```text\n{result}\n``` " , parse_mode=ParseMode.MARKDOWN)
+                
+                
+            except Exception as e:
+                await message.reply_text(f"Error in calculation: {e}")
+                pass
+        print(f"Received message: {message.text}")
     
-    
+
     
 app.run()
